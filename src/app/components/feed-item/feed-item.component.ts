ab492fdf955093bc059d8ecb694bea3c6cfe9e1d
amandajcustodio <amandajcustodio@outlook.com>
2022-07-24 12:28:20 -0300

    update(home): added delete button and create(feed): feed page and comments

---
+++ src/app/components/feed-item/feed-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { PostItProxy } from 'src/app/models/proxies/postit.proxy';
+
+@Component({
+  selector: 'app-feed-item',
+  templateUrl: './feed-item.component.html',
+  styleUrls: ['./feed-item.component.scss'],
+})
+export class FeedItemComponent implements OnInit {
+
+  constructor() { }
+
+  @Input()
+  public postIt: PostItProxy;
+
+  public isLiked: boolean = false;
+
+  ngOnInit() {}
+
+  public setLikeToPostIt(): void{
+    this.isLiked = !this.isLiked;
+  }
+}
